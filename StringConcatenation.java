0ab00b215803316b9bfe6e0f8d21e84f0dc22de0
RupaSri Pitchuka <24BQ5A1213@vvit.net>
2026-07-03 19:20:33 +0530

    Add files via upload

---
+++ StringConcatenation.java
@@ -0,0 +1,9 @@
+class StringConcatenation {
+    public static void main(String args[]) {
+        if (args.length != 2) {
+            System.out.println("Please pass two command line arguments");
+        } else {
+            System.out.println(args[0] + " Technologies " + args[1]);
+        }
+    }
+}
