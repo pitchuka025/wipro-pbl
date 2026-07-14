ac08b8f81f85f0d2f0e160b02c2110faeecaecac
RupaSri Pitchuka <24BQ5A1213@vvit.net>
2026-07-14 09:15:29 +0530

    Add files via upload

---
+++ LogicalBuilding/Day5/IsExactMultiple.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class IsExactMultiple {
+
+    public static int isExactMultiple(int n, int m) {
+        if (m == 0)
+            return 1; // Invalid case
+        
+        if (n % m == 0)
+            return 2; // Yes, N is an exact multiple of M
+        else
+            return 1; // No, N is not an exact multiple of M
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter M: ");
+        int m = sc.nextInt();
+
+        System.out.println("Output: " + isExactMultiple(n, m));
+
+        sc.close();
+    }
+}
