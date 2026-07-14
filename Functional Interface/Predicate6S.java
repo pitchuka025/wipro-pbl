4b5fd48e21991268cdd64650b93c1133d3ae6e01
RupaSri Pitchuka <24BQ5A1213@vvit.net>
2026-07-14 20:41:47 +0530

    Add files via upload

---
+++ Functional Interface/Predicate6S.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class Predicate6S {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>(Arrays.asList(
+                "java", "python", "c", "spring", "html",
+                "css", "sql", "oracle", "ruby", "kotlin"
+        ));
+
+        for (int i = 0; i < words.size(); i++) {
+            final int index = i;
+            Consumer<String> reverseAndUpdate = word -> words.set(index, new StringBuilder(word).reverse().toString());
+            reverseAndUpdate.accept(words.get(index));
+        }
+
+        System.out.println("Reversed words: " + words);
+    }
+}
